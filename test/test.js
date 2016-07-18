14253571da1c5270c11478b82e39636ec488d5e3
Sherry Yang <sherry.yang@sait.ca>
2016-07-18 12:51:37 -0600

    modified test case 1

--- test/test.js
+++ test/test.js
@@ -8,39 +8,4 @@ describe('Array', function() {
     });
   });
 
-  describe('#indexOf()2', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-
-
-});
-
-describe('Array3', function() {
-  describe('#indexOf()3', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
-
-describe('Array4', function() {
-  describe('#indexOf()4', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(1, [1,2,3].indexOf(5));
-      assert.equal(1, [1,2,3].indexOf(0));
-    });
-  });
-});
-
-describe('Array5', function() {
-  describe('#indexOf()5', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
 });
