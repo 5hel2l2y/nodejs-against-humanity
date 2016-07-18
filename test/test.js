7e046697db4b1ff378d055d578f61338860aab14
Sherry Yang <sherry.yang@sait.ca>
2016-07-18 15:01:44 -0600

    moar tests

--- test/test.js
+++ test/test.js
@@ -12,8 +12,8 @@ describe('Array', function() {
 describe('Array2', function() {
   describe('#indexOf()', function() {
     it('should return -2 when the value is not present WORK DAMN YOU\n', function() {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+      assert.equal(-1, [1,2,3].indexOf(2));
+      assert.equal(-1, [1,2,3].indexOf(3));
     });
   });
 });
@@ -21,8 +21,8 @@ describe('Array2', function() {
 describe('Array3', function() {
   describe('#indexOf()', function() {
     it('should return -3 when the value is not present WORK DAMN YOU\n', function() {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+      assert.equal(1, [1,2,3].indexOf(1));
+      assert.equal(2, [1,2,3].indexOf(2));
     });
   });
 });
