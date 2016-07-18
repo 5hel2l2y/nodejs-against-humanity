e7a1e38faa98ea253fa0dab8a8344da0010a9b36
Sherry Yang <sherry.yang@sait.ca>
2016-07-18 12:36:36 -0600

    modified test case 1

--- test/test.js
+++ test/test.js
@@ -7,15 +7,15 @@ describe('Array', function() {
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
-});
 
-describe('Array2', function() {
   describe('#indexOf()2', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
+
+
 });
 
 describe('Array3', function() {
