379ff379acf02904bcbce53954ca52d222c1fc10
Sherry Yang <sherry.yang@sait.ca>
2016-07-18 14:58:56 -0600

    add more tests

--- test/test.js
+++ test/test.js
@@ -3,9 +3,44 @@ var assert = require('chai').assert;
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present WORK DAMN YOU\n', function() {
-      // assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
+});
 
+describe('Array2', function() {
+  describe('#indexOf()', function() {
+    it('should return -2 when the value is not present WORK DAMN YOU\n', function() {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+describe('Array3', function() {
+  describe('#indexOf()', function() {
+    it('should return -3 when the value is not present WORK DAMN YOU\n', function() {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+describe('Array4', function() {
+  describe('#indexOf()', function() {
+    it('should return -4 when the value is not present WORK DAMN YOU\n', function() {
+      assert.equal(100, [1,2,3].indexOf(5));
+      assert.equal(40, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+describe('Array5', function() {
+  describe('#indexOf()', function() {
+    it('should return -5 when the value is not present WORK DAMN YOU\n', function() {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
 });
