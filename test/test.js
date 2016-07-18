9fd76916f8fc3120f06c6de476c6ebe92d5e71ac
Sherry Yang <sherry.yang@sait.ca>
2016-07-18 15:06:55 -0600

    moar tests

--- test/test.js
+++ test/test.js
@@ -12,8 +12,8 @@ describe('Array', function() {
 describe('Array2', function() {
   describe('#indexOf()', function() {
     it('should return -2 when the value is not present WORK DAMN YOU\n', function() {
-      assert.equal(-1, [1,2,3].indexOf(2));
-      assert.equal(-1, [1,2,3].indexOf(3));
+      assert.equal(1, [1,2,3].indexOf(2));
+      assert.equal(2, [1,2,3].indexOf(3));
     });
   });
 });
@@ -22,7 +22,7 @@ describe('Array3', function() {
   describe('#indexOf()', function() {
     it('should return -3 when the value is not present WORK DAMN YOU\n', function() {
       assert.equal(0, [1,2,3].indexOf(1));
-      assert.equal(2, [1,2,3].indexOf(2));
+      assert.equal(1, [1,2,3].indexOf(2));
     });
   });
 });
@@ -30,8 +30,8 @@ describe('Array3', function() {
 describe('Array4', function() {
   describe('#indexOf()', function() {
     it('should return -4 when the value is not present WORK DAMN YOU\n', function() {
-      assert.equal(100, [1,2,3].indexOf(5));
-      assert.equal(40, [1,2,3].indexOf(0));
+      assert.equal(-1, [1,2,3].indexOf(50));
+      assert.equal(-1, [1,2,3].indexOf(-2));
     });
   });
 });
