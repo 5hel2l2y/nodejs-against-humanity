34ee7880a20006e00f3a88a8c0f19062a7c25092
Sherry Yang <sherry.yang@sait.ca>
2016-07-19 10:07:07 -0600

    pass all tests

--- test-tape.js
+++ test-tape.js
@@ -61,10 +61,10 @@ test('A passing test', (assert) => {
 
 test('Assertions with tape.', (assert) => {
   const expected = 'something to test';
-  const actual = 'sonething to test';
+  const actual = 'something to test';
 
   assert.equal(actual, expected,
-    'Given two mismatched values, .equal() should produce a nice bug report');
+    'Given two mismatched values, .equal() should produce a nice bug report. NOW IS THE SAME.');
 
   assert.end();
 });
